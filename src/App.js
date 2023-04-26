225ae17c7eeee962ba2488bb8ae3a5d567e446d9
marielacordeiro <mariela_pontes_corde@dellteam.com>
2023-04-26 14:59:51 -0300

    set react for the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       AGENDAMENTO DE CHURRAS
+      </header>
+    </div>
+  );
+}
+export default App;
